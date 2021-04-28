058c76ff1dad11ae4218819212f3eb3b7fc82228
Patricia Molnar <m.patricia@live.com>
2021-04-28 18:48:05 +0100

    changing connecting to DB: app only starts listening after successful connection to DB

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express'); 
 const mongoose = require('mongoose'); 
 require('dotenv').config();
+const db = require('./lib/db');
 
 const app = express();
 app.use(express.json());
 
 const port = 5000;
 
-mongoose.connect(process.env.DEVELOPMENT_DB_DSN, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-    console.log('successfully connected to database');
-});
+db.connect(process.env.DEVELOPMENT_DB_DSN).
+    then(() => {
+        console.log('Conntected to MongoDB'); 
+        app.listen(port, () => {
+                console.log('Express server started');
+            });
+    }).catch((err) => {
+        console.log(err);
+    });
 
-app.listen(port, () => {
-    console.log('Express server started');
-})
