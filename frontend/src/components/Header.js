f4f3a815bcffbad14ab548c7038d2fb4156de3c5
Patricia Molnar <m.patricia@live.com>
2021-05-09 15:08:50 +0100

    correcting text in Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
     return(
         <header>
-            <h1>Conjugateau</h1>
-            <p>Makes learning French conjugation a piece of cake!</p>
+            <h1>Conjugâteau</h1>
+            <p>Making learning French conjugation a piece of cake!</p>
         </header>
     )
 }
