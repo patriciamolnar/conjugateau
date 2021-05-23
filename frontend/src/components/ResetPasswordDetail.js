83f98f883230058e540cb9ce75588484fa3a9ae2
Patricia Molnar <m.patricia@live.com>
2021-05-23 11:48:10 +0100

    refactoring ResetPasswordDetail.js

--- frontend/src/components/ResetPasswordDetail.js
+++ frontend/src/components/ResetPasswordDetail.js
@@ -1,62 +1,69 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useInput } from '../lib/customHooks';
+import { handleSubmit } from '../lib/fetch';
+import ToggleVisibility from './ToggleVisibility';
 
 function ResetPasswordDetail() {
-    const [password, setPassword] = useState({
-        newPass: '', 
-        oldPass: ''
-    });
-
+    const [oldPassProps, resetOldPass] = useInput(''); 
+    const [newPassProps, resetNewPass] = useInput('');
+    const [loading, setLoading] = useState(false);
     const [showPass, setShowPass] = useState(false);
     const [message, setMessage] = useState(null);
 
-    const updateDetails = (e) => {
-        const inputName = e.target.name; 
-        const inputValue = e.target.value; 
-        setPassword(prevState => {
-            return {...prevState, [inputName]: inputValue}
+    const changePassword = (e) => {
+        const obj = {
+            uri: '/user/password/', 
+            method: 'PUT',
+            details: {
+                oldPass: oldPassProps.value,
+                newPass: newPassProps.value
+            }
+        }
+
+        setLoading(true);
+
+        handleSubmit(e, obj).then(data => {
+            resetOldPass(); //resetting state and wih it the input fields.
+            resetNewPass();
+            setLoading(false);
+
+            if(data.success) {
+                setMessage(data.message);
+            } else {
+                setMessage(data.message);
+            } 
+        })
+        .catch(err => {
+            setMessage('An error occured. Please try again later.');
         });
     }
 
-    const changePassword = (e) => {
-        e.preventDefault(); 
-        fetch('/user/password/', {
-            method: "PUT",
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(password)
-            })
-            .then(res => res.json())
-            .then(data => {
-                if(data.success) {
-                    setMessage(data.message);
-                    setPassword({oldPass: '', newPass: ''}); 
-                } else {
-                    setMessage(data.message);
-                } 
-            })
-            .catch(err => {
-                setMessage('An error occured. Please try again later.');
-            });
+    if(loading) {
+        return <p>Loading...</p>;
     }
 
     return(
         <div>
             <h2>Change your password:</h2>
-            {message ? message : null}
+            <p>{message && message}</p>
             <form onSubmit={(e) => changePassword(e)}>
-                <label htmlFor="password">Your old password:</label>
-                <input type={showPass ? "text" : "password"} value={password.oldPass} name="oldPass" onChange={(e) => updateDetails(e)} autoComplete="current-password"/>
-                <label htmlFor="password">Your new password:</label>
-                <input type={showPass ? "text" : "password"} value={password.newPass} name="newPass" onChange={(e) => updateDetails(e)} autoComplete="new-password"/>
-                <label htmlFor="toggle-password-visibility">
-                    <input 
-                        type="checkbox" 
-                        id="toggle-password-visibility" 
-                        checked={showPass}
-                        onChange={() => setShowPass(!showPass)}/>
-                    Show password
-                </label>
+
+                <label htmlFor="old-password">Your old password:</label>
+                <input { ...oldPassProps }
+                    type={showPass ? "text" : "password"} 
+                    id="old-password" 
+                    name="old-password"
+                    autoComplete="current-password"/>
+
+                <label htmlFor="new-password">Your new password:</label>
+                <input { ...newPassProps }
+                    type={showPass ? "text" : "password"} 
+                    id="new-password"
+                    name="new-password" 
+                    autoComplete="new-password"/>
+
+                <ToggleVisibility id={"change-password"} showPass={showPass} setShowPass={setShowPass} />
+
                 <button type="submit">Change</button>
             </form>
         </div>
