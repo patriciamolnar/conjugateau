12ec61089cdfb92a51d4ac4135410b8d7fdd1961
Patricia Molnar <m.patricia@live.com>
2021-05-29 12:44:44 +0100

    adding styling to forgotten password page

--- frontend/src/pages/ForgottenPassword.js
+++ frontend/src/pages/ForgottenPassword.js
@@ -3,6 +3,7 @@ import { useInput } from '../lib/customHooks';
 import { handleSubmit } from '../lib/fetch';
 import { formatInput } from "../lib/functions";
 import Loading from '../components/Loading';
+import { Link } from 'react-router-dom';
 
 function ForgottenPassword() {
     const [emailProps, resetEmail] = useInput('');
@@ -42,19 +43,21 @@ function ForgottenPassword() {
     }
 
     return(
-        <>
-        <h2>Reset your password</h2>
-        <p>Please enter your email below.</p>
-        {message && <p className={success ? 'correct' : 'false'}>{message}</p>}
-        <form onSubmit={(e) => requestReset(e)}>
-            <label htmlFor="reset-email"></label>
-            <input {...emailProps}
-                type="email" 
-                id="reset-email" 
-                autoComplete="email"/>
-            <button type="submit">Send</button>
-        </form>
-        </>
+        <div className="password">
+            <h2>Reset your password</h2>
+            <p>Please enter your email below. We will then send you an email with a password reset link.</p>
+            {message && <p className={success ? 'correct' : 'false'}>{message}</p>}
+            <form onSubmit={(e) => requestReset(e)}>
+                <label htmlFor="reset-email"></label>
+                <input {...emailProps}
+                    type="email" 
+                    id="reset-email" 
+                    autoComplete="email"/>
+                <button type="submit">Send</button>
+            </form>
+
+            <Link to="/account">Login or Register here</Link>
+        </div>
     );
 }
 
