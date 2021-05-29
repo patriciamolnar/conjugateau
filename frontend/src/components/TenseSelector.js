20da11b2fd30293e1486f5629e3ba1ff194efd8c
Patricia Molnar <m.patricia@live.com>
2021-05-29 15:44:10 +0100

    adding all tenses to TenseSelector

--- frontend/src/components/TenseSelector.js
+++ frontend/src/components/TenseSelector.js
@@ -1,18 +1,32 @@
 function TenseSelector({ updateOptions, startGame }) { 
-    const tenseNames = ['present-indicative', 'imparfait'];
+    const tenseNames = [
+        'present-indicative', 
+        'imparfait', 
+        'future', 
+        'present-conditional', 
+        'passé-composé', 
+        'pluperfect',
+        'future-perfect',
+        'past-conditional',
+        'present-subjunctive', 
+        'past-subjunctive'
+    ];
 
     return(
         <div className="tense-selector">
             <p>Select tenses to practice:</p>
-            {tenseNames.map(name => {
-                return( 
-                    <label htmlFor={name} key={name}>
-                        <input id={name} name={name} type="checkbox" onClick={(e) => updateOptions(e.target.name)}/>
-                        {name}
-                    </label>
-                )
-            })}
-            <button onClick={startGame}>Start</button>
+            <form>
+                {tenseNames.map(name => {
+                    return( 
+                        <label htmlFor={name} key={name}>
+                            <input id={name} name={name} type="checkbox" onClick={(e) => updateOptions(e.target.name)}/>
+                            {name}
+                        </label>
+                    )
+                })}
+                <br />
+                <button type="button" onClick={startGame}>Start</button>
+            </form>
         </div>
     )
 }
