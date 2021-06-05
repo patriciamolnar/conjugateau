257632623fc2474d984a21b330942e02b8d23461
Patricia Molnar <m.patricia@live.com>
2021-06-05 09:47:59 +0100

    adding process.env port and scripts to backend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./lib/db');
 const userRouter = require('./routes/UserRouter');
 const verbRouter = require('./routes/VerbRouter');
 const errorHandler = require('./errorHandler');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
@@ -15,6 +15,16 @@ app.use(cookies());
 
 app.use('/user', userRouter);
 app.use('/verbs', verbRouter); 
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+    
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+  }
 
 db.connect(process.env.DEVELOPMENT_DB_DSN).
     then(() => {
